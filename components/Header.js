8d175f19d8e3c45b096667f868bbe29a6cf91d46
md-huzaifa <89762321+md-huzaifa@users.noreply.github.com>
2022-03-06 03:02:59 +0530

    created the landing page

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,40 @@
+import Link from "next/link";
 import styles from "../styles/header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>LOGO</div>
-      <div className={styles.item}>Home</div>
-      <div className={styles.item}>
-        <div class={styles.dropdown}>
-          <button class={styles.dropbtn}>Dropdown</button>
-          <div class={styles.dropdownContent}>
-            <a href="#">Health</a>
-            <a href="#">Face Detection</a>
-            <a href="#">Speech Synthesis</a>
-            <a href="#">Vision Systems</a>
+    <>
+      <div className={styles.container}>
+        <div className={styles.item}>LOGO</div>
+        <div className={styles.item}>
+          <Link href="/">
+            <a style={{ color: "white", textDecoration: "none" }}>Home</a>
+          </Link>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.dropdown}>
+            <button className={styles.dropbtn}>Dropdown</button>
+            <div className={styles.dropdownContent}>
+              <a href="#">Health Care</a>
+              <a href="#">Face Detection</a>
+              <a href="#">Speech Synthesis</a>
+              <a href="#">Vision Systems</a>
+            </div>
           </div>
         </div>
+        <div className={styles.item}>
+          <Link href="/contact">
+            <a style={{ color: "white", textDecoration: "none" }}>Contact</a>
+          </Link>
+        </div>
+        <div className={styles.item}>
+          <Link href="/aboutus">
+            <a style={{ color: "white", textDecoration: "none" }}>About Us</a>
+          </Link>
+        </div>
       </div>
-      <div className={styles.item}>Contact</div>
-      <div className={styles.item}>About Us</div>
-    </div>
+      {props.children}
+    </>
   );
 };
 
